a608ba020d7e8c33014d976b850fd7fd50a21c18
santhoshwesley <santhoshwesleyyer@gmail.com>
2024-02-22 14:01:36 +0530

    counter

---
+++ xcounter/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
